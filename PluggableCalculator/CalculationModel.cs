c59e3e2d1aa3e362e8c562d970529330d608e2bf
Antonio Elena <antonioelena@gmail.com>
2014-07-06 05:15:39 +0200

    PluggableCAlculator demo - interesting

---
+++ PluggableCalculator/CalculationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PluggableCalculator
+{
+    class CalculationModel
+    {
+        public int Input1 { get; set; }
+
+        public int Input2 { get; set; }
+
+        public string Op { get; set; }
+    }
+}
---
+++ PluggableCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluggableCalculator
+{
+    class Program
+    {
+        static void Main ( string [] args )
+        {
+            var catalog = new AssemblyCatalog ( Assembly.GetExecutingAssembly () );
+            CompositionContainer container = new CompositionContainer ( catalog );
+
+            var calculator = container.GetExportedValue<Calculator> ();
+
+            // set up a loop of calculations
+            while(true)
+            {
+                Console.WriteLine ("Enter your calculation here: ");
+                string input = Console.ReadLine ();
+
+                Console.WriteLine ( "Result: {0}", calculator.Calculate ( input ) );
+            }
+        }
+    }
+}
